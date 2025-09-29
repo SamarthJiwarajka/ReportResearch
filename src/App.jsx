c607d551a59902f3e2c657cfc4fbc206f177faa2
SamarthJiwarajka <samjiv@gmail.com>
2025-09-29 11:30:43 +0530

    Updated Similarity Score

--- src/App.jsx
+++ src/App.jsx
@@ -400,7 +400,7 @@ const App = () => {
                 
                 if (docVector && typeof docVector === 'object') {
                     const similarityScore = cosineSimilarity(queryVector, docVector);
-                    if (similarityScore > 0.25) { 
+                    if (similarityScore > 0.275) { 
                         retrievedReports.push({ 
                             id: doc.id, 
                             ...report, 
